417eb88b31c8ed2f833ac217eba48bda7e9d1731
Luís Diogo <luis.manuel.diogo@gmail.com>
2023-03-02 13:49:26 +0000

    erro4 5incompleto

--- src/aula3/Ex5.java
+++ src/aula3/Ex5.java
@@ -1,5 +1,30 @@
 package aula3;
 
+//Escreva um programa que lê do teclado uma data no formato mm/yyyy (validando os valores) 
+//e o dia da semana em que começa esse mês (1 = Segunda, 2 = Terça, 3 = Quarta, 4 = Quinta, 
+//5 = Sexta, 6 = Sábado, 7 = Domingo). O programa deve depois apresentar no monitor o 
+//calendário desse mês com o formato que a seguir se apresenta. 
+//Implemente a funcionalidade pretendida com três funções: leitura de valores, com validação, 
+//cálculo de dias no mês, considerando os anos bissextos, e impressão de resultados
+
+import java.util.Scanner;
+
 public class Ex5 {
+
+	public static void leituraV(){
+		String mmyy;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.print("Data no formato mm/yy:");
+			mmyy = sc.nextLine();
+		} while 
+		
+
+	}
+	public static void main(String[] args) {
+
+		leituraV();
+
+	}
     
 }
